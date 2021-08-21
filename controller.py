95f81efe79adc758abef3479a87f6f5054dccd73
THEBR4Y4N <39465767+THEBR4Y4N@users.noreply.github.com>
2021-08-21 15:58:19 -0500

    Fase 3
    fase 3

--- controller.py
+++ controller.py
@@ -1,5 +1,41 @@
+#Controller
+import re
+from tkinter import *
+class Validador:
+    
 
-import model
-
-
-model.prueba()
+    def validador_nombres(self, Ingreso):
+        regex0 = re.compile(Ingreso.getRegex1())
+        print(f"regex: {regex0}")
+        cadena0 = (Ingreso.getEntrada1()) 
+        print(f"cadena: {cadena0}")
+        Ingreso.setEntrada1("")
+        resultado = regex0.match(cadena0)
+        if bool(resultado) == True:
+            return True
+        else:
+            return False
+    
+    def validador_numeros(self, Ingreso):
+        regex1 = re.compile(Ingreso.getRegex2())
+        print(f"regex: {regex1}")
+        cadena1 = Ingreso.getEntrada2()
+        print(f"cadena: {cadena1}")
+        Ingreso.setEntrada2("")
+        resultado = regex1.match(cadena1)
+        if bool(resultado) == True:
+            return True
+        else:
+            return False
+            
+    def validador_correos(self, Ingreso):
+        regex2 = re.compile(Ingreso.getRegex3())
+        print(f"regex: {regex2}")
+        cadena2 = Ingreso.getEntrada3()
+        print(f"cadena: {cadena2}")
+        Ingreso.setEntrada3("")
+        resultado = regex2.match(cadena2)
+        if bool(resultado) == True:
+            return True
+        else:
+            return False
--- model.py
+++ model.py
@@ -1,4 +1,39 @@
+#model
+class Ingreso():
+    
+    __entrada1 = ""
+    __entrada2 = ""
+    __entrada3 = ""
+    __regex1 = "[a-zA-Z]"
+    __regex2 = "^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$"
+    __regex3 = "^[^@]+@[^@]+\.[a-zA-Z]{2,}$"
 
-class Model:
-    def prueba():
-        print("prueba")
+    #Getters
+    def getEntrada1(self):
+        return self.__entrada1
+
+    def getEntrada2(self):
+        return self.__entrada2
+
+    def getEntrada3(self):
+        return self.__entrada3
+        
+    def getRegex1(self):
+        return self.__regex1
+
+    def getRegex2(self):
+        return self.__regex2
+    
+    def getRegex3(self):
+        return self.__regex3
+
+    #Setters
+    def setEntrada1(self, param):
+        self.__entrada1 = param
+
+    def setEntrada2(self, param):
+        self.__entrada2 = param
+    
+    def setEntrada3(self, param):
+        self.__entrada3 = param
+
